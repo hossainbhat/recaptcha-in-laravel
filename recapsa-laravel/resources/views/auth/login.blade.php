a728f118689258fbcc1a19e22fa07767283a981b
Md.Hossain Bhat <40881808+hossainbhat@users.noreply.github.com>
2022-03-01 23:22:12 +0600

    install recapsa

--- recapsa-laravel/resources/views/auth/login.blade.php
+++ recapsa-laravel/resources/views/auth/login.blade.php
@@ -42,11 +42,13 @@
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                    {!! NoCaptcha::renderJs() !!}
+                                    {!! NoCaptcha::display() !!}
+                                    @if ($errors->has('g-recaptcha-response'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                                        </span>
+                                    @endif
                                 </div>
                             </div>
                         </div>
